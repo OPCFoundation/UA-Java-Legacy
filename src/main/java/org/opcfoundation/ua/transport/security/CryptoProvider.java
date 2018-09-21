c379e44fe3465b575e52fb740d5a2111c6f0efb8
Bjakke <bjakke@prosys.fi>
2018-09-21 10:25:24 +0300

    Removed old non-informative autogenerated javadocs

--- src/main/java/org/opcfoundation/ua/transport/security/CryptoProvider.java
+++ src/main/java/org/opcfoundation/ua/transport/security/CryptoProvider.java
@@ -19,145 +19,45 @@ import javax.crypto.Mac;
 import org.opcfoundation.ua.common.ServiceResultException;
 
 /**
- * Crypto Provider interface for encrypting and decrypting services.
+ * Interface for encrypting and decrypting services.
+ * Designed for stack internal use to allow pluggable system of providers.
  */
 public interface CryptoProvider {
 
-	/**
-	 * <p>base64Decode.</p>
-	 *
-	 * @param string a {@link java.lang.String} object.
-	 * @return an array of byte.
-	 */
 	public byte[] base64Decode(String string);
 
-	/**
-	 * <p>base64Encode.</p>
-	 *
-	 * @param bytes an array of byte.
-	 * @return a {@link java.lang.String} object.
-	 */
 	public String base64Encode(byte[] bytes);
 
-	/**
-	 * <p>createMac.</p>
-	 *
-	 * @param algorithm a {@link org.opcfoundation.ua.transport.security.SecurityAlgorithm} object.
-	 * @param secret an array of byte.
-	 * @return a {@link javax.crypto.Mac} object.
-	 * @throws org.opcfoundation.ua.common.ServiceResultException if any.
-	 */
 	public Mac createMac(SecurityAlgorithm algorithm, byte[] secret)
 			throws ServiceResultException;
 
-	/**
-	 * <p>decryptAsymm.</p>
-	 *
-	 * @param decryptingKey a {@link java.security.PrivateKey} object.
-	 * @param algorithm a {@link org.opcfoundation.ua.transport.security.SecurityAlgorithm} object.
-	 * @param dataToDecrypt an array of byte.
-	 * @param output an array of byte.
-	 * @param outputOffset a int.
-	 * @return a int.
-	 * @throws org.opcfoundation.ua.common.ServiceResultException if any.
-	 */
 	public int decryptAsymm(PrivateKey decryptingKey,
 			SecurityAlgorithm algorithm, byte[] dataToDecrypt, byte[] output,
 			int outputOffset) throws ServiceResultException;
 
-	/**
-	 * <p>decryptSymm.</p>
-	 *
-	 * @param token a {@link org.opcfoundation.ua.transport.tcp.impl.SecurityToken} object.
-	 * @param dataToDecrypt an array of byte.
-	 * @param inputOffset a int.
-	 * @param inputLength a int.
-	 * @param output an array of byte.
-	 * @param outputOffset a int.
-	 * @return a int.
-	 * @throws org.opcfoundation.ua.common.ServiceResultException if any.
-	 */
 	public int decryptSymm(SecurityPolicy policy, byte[] encryptingKey, byte[] initializationVector, byte[] dataToDecrypt,
 			int inputOffset, int inputLength, byte[] output, int outputOffset)
 					throws ServiceResultException;
 
-	/**
-	 * <p>encryptAsymm.</p>
-	 *
-	 * @param encryptingCertificate a {@link java.security.PublicKey} object.
-	 * @param algorithm a {@link org.opcfoundation.ua.transport.security.SecurityAlgorithm} object.
-	 * @param dataToEncrypt an array of byte.
-	 * @param output an array of byte.
-	 * @param outputOffset a int.
-	 * @throws org.opcfoundation.ua.common.ServiceResultException if any.
-	 */
 	public void encryptAsymm(PublicKey encryptingCertificate,
 			SecurityAlgorithm algorithm, byte[] dataToEncrypt, byte[] output,
 			int outputOffset) throws ServiceResultException;
 
-	/**
-	 * <p>encryptSymm.</p>
-	 *
-	 * @param token a {@link org.opcfoundation.ua.transport.tcp.impl.SecurityToken} object.
-	 * @param dataToEncrypt an array of byte.
-	 * @param inputOffset a int.
-	 * @param inputLength a int.
-	 * @param output an array of byte.
-	 * @param outputOffset a int.
-	 * @return a int.
-	 * @throws org.opcfoundation.ua.common.ServiceResultException if any.
-	 */
 	public int encryptSymm(SecurityPolicy policy, byte[] encryptingKey, byte[] initializationVector, byte[] dataToEncrypt,
 			int inputOffset, int inputLength, byte[] output, int outputOffset)
 					throws ServiceResultException;
 
-	/**
-	 * <p>signAsymm.</p>
-	 *
-	 * @param senderPrivate a {@link java.security.PrivateKey} object.
-	 * @param algorithm a {@link org.opcfoundation.ua.transport.security.SecurityAlgorithm} object.
-	 * @param dataToSign an array of byte.
-	 * @return an array of byte.
-	 * @throws org.opcfoundation.ua.common.ServiceResultException if any.
-	 */
 	public byte[] signAsymm(PrivateKey senderPrivate,
 			SecurityAlgorithm algorithm, byte[] dataToSign)
 					throws ServiceResultException;
 
-	/**
-	 * <p>signSymm.</p>
-	 *
-	 * @param token a {@link org.opcfoundation.ua.transport.tcp.impl.SecurityToken} object.
-	 * @param input an array of byte.
-	 * @param verifyLen a int.
-	 * @param output an array of byte.
-	 * @throws org.opcfoundation.ua.common.ServiceResultException if any.
-	 */
 	public void signSymm(SecurityPolicy policy, byte[] key, byte[] input, int inputOffset, int verifyLen,
 			byte[] output) throws ServiceResultException;
 
-	/**
-	 * <p>verifyAsymm.</p>
-	 *
-	 * @param signingCertificate a {@link java.security.PublicKey} object.
-	 * @param algorithm a {@link org.opcfoundation.ua.transport.security.SecurityAlgorithm} object.
-	 * @param dataToVerify an array of byte.
-	 * @param signature an array of byte.
-	 * @return a boolean.
-	 * @throws org.opcfoundation.ua.common.ServiceResultException if any.
-	 */
 	public boolean verifyAsymm(PublicKey signingCertificate,
 			SecurityAlgorithm algorithm, byte[] dataToVerify, byte[] signature)
 					throws ServiceResultException;
 
-	/**
-	 * <p>verifySymm.</p>
-	 *
-	 * @param token a {@link org.opcfoundation.ua.transport.tcp.impl.SecurityToken} object.
-	 * @param dataToVerify an array of byte.
-	 * @param signature an array of byte.
-	 * @throws org.opcfoundation.ua.common.ServiceResultException if any.
-	 */
 	public void verifySymm(SecurityPolicy policy, byte[] key, byte[] dataToVerify, int inputOffset, int verifyLen,
 			byte[] signature) throws ServiceResultException;
 
